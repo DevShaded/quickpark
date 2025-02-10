ecbda823c5089c4d402521ac0955f5bef8d78fd9
DevShaded <devshaded@devshaded.com>
2025-02-10 13:31:31 +0100

    feat(feature/frontend): add default layout

---
+++ resources/js/Layouts/DefaultLayout.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import Navbar from "@/Components/Navbar.vue";
+</script>
+
+<template>
+    <div>
+        <header>
+            <Navbar />
+        </header>
+
+        <main class="container mx-auto px-4 pb-12 lg:px-8 xl:max-w-7xl">
+            <slot />
+        </main>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
