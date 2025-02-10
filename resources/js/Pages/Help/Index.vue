a40eb309cfde7e528bfd7c797410d869beee7389
DevShaded <devshaded@devshaded.com>
2025-02-10 13:38:04 +0100

    feat(feature/frontend): add help page TODO

---
+++ resources/js/Pages/Help/Index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import DefaultLayout from "@/Layouts/DefaultLayout.vue";
+</script>
+
+<template>
+    <DefaultLayout>
+        <h1 class="text-2xl font-semibold text-gray-900">Help Page</h1>
+    </DefaultLayout>
+</template>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use App\Http\Controllers\ParkingController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', [ParkingController::class, 'index'])->name('home');
 Route::post('/park', [ParkingController::class, 'store'])->name('park');
+
+Route::get('/help', function () {
+    return Inertia::render('Help/Index');
+})->name('help');
