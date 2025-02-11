bb0c3b9b8613efe4df18b6e0883c781d83178713
DevShaded <devshaded@devshaded.com>
2025-02-11 10:38:29 +0100

    refactor(feature/frontend): extract success and error to components

--- resources/js/Components/RegisterParkingForm.vue
+++ resources/js/Components/RegisterParkingForm.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import ErrorCard from "@/Components/ErrorCard.vue";
+import SuccessCard from "@/Components/SuccessCard.vue";
 import { useForm, usePage } from '@inertiajs/vue3';
 import { XCircleIcon, CheckCircleIcon } from '@heroicons/vue/20/solid'
 
@@ -76,23 +78,12 @@ const submitForm = () => {
             </div>
         </div>
 
-        <div
-            v-if="page.props.flash.success"
-            class="flex items-center justify-center my-12"
-        >
-            <div class="rounded-md bg-green-50 p-4">
-                <div class="flex">
-                    <div class="shrink-0">
-                        <CheckCircleIcon class="size-5 text-green-400" aria-hidden="true" />
-                    </div>
-                    <div class="ml-3">
-                        <h3 class="text-sm font-medium text-green-800">Din bil har nå blitt registrert!</h3>
-                        <div class="mt-2 text-sm text-green-700">
-                            <p class="font-medium">{{ page.props.flash.success }}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div v-if="page.props.flash.success">
+            <SuccessCard :success="page.props.flash.success" />
+        </div>
+
+        <div v-if="page.props.flash.error">
+            <ErrorCard :error="page.props.flash.error" />
         </div>
 
 
