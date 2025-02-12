0361915b031b984f87843ac808d546853bdae2bb
DevShaded <devshaded@devshaded.com>
2025-02-12 10:03:57 +0100

    feat(feature/delete-expired-parking-assignments): add job for deleting expired parking assignments

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Jobs\DeleteExpiredParkingAssignmentsJob;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
+    })
+    ->withSchedule(function ($schedule) {
+        $schedule->job(new DeleteExpiredParkingAssignmentsJob)->everyTwoHours();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
