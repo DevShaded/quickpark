907fa6a029f85beee83585b92eb2d5508d0322eb
DevShaded <devshaded@devshaded.com>
2025-02-12 08:16:08 +0100

    feat(feature/frontend): hide parking slots when there is no parking slots left

--- resources/js/Components/ParkingSlots.vue
+++ resources/js/Components/ParkingSlots.vue
@@ -16,7 +16,7 @@ defineProps<{
     <div class="max-w-xl mx-auto mt-16">
         <p class="text-lg text-gray-600">Parkeringsplasser som er ledig:</p>
         <ul
-            v-if="parkingSlots.data"
+            v-if="parkingSlots.length > 0"
             class="divide-y divide-gray-200 rounded-lg border border-gray-200 bg-white"
         >
             <li v-for="slot in parkingSlots" class="flex items-center justify-between p-4">
@@ -24,7 +24,7 @@ defineProps<{
                 <span class="text-sm font-medium text-emerald-500">{{ slot.status }}</span>
             </li>
         </ul>
-        <p v-else class="text-lg text-gray-600 font-bold">Ingen parkeringsplasser er ledig</p>
+        <p v-else class="text-lg text-gray-600 font-bold">Ingen parkeringsplasser er ledig :(</p>
     </div>
 </template>
 
